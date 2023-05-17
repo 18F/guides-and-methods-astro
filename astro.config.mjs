7b85a0b7ba84f9f88de09b98e7476be243ac01d1
Julia Allen <julia.l.allen@gsa.gov>
2023-05-17 09:58:25 -0700

    add MDX plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   base: process.env.BASEURL,
-  outDir: './_site'
+  outDir: './_site',
+  integrations: [mdx()]
 });
